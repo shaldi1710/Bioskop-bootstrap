4a5e33a7990c8928832c9c1bd0fd6a9c9477e7e9
shaldi1710 <faishalhadi.hf@gmail.com>
2023-02-22 19:52:56 +0700

    update

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,6 @@
 import { Col, Container, Row, Button } from 'react-bootstrap';
 const Intro = () => {
+  
     return(
         <div className="intro">
           <Container className="text-white text-center d-flex justify-content-center align-items-center">
